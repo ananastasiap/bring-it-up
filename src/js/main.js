5bbd19fd302ee96b6ed0e6f6446b051dc48c1f2f
ananastasiap <anestheziia@gmail.com>
2023-07-18 23:21:33 +0300

    feat: video player

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
-import { Slider } from "./modules/index.js";
+import { Slider , VideoPlayer} from "./modules/index.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new Slider('.page', '.next');
   slider.render();
+
+  const player = new VideoPlayer('.showup .play', '.overlay');
+  player.init();
 });
