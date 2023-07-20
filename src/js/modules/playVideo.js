3e15bd7fcd7a7e19e45b416c736abc23ca407836
ananastasiap <anestheziia@gmail.com>
2023-07-20 09:40:05 +0300

    refactor: createVideo

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -37,11 +37,11 @@ export class VideoPlayer {
     });
   }
 
-  createPlayer(url) {
+  createPlayer(videoId) {
     this.player = new YT.Player('frame', {
       height: '100%',
       width: '100%',
-      videoId: `${url}`,
+      videoId,
     });
 
     console.log(this.player);
